90c6f88bf65f59a0adf075482a00d4bba4e0397f
Eduardo Pedro <eduardopedro.fsa@gmail.com>
2025-11-17 12:08:59 -0300

    Configure ESLint v9 with TypeScript support
    - Add eslint.config.js with flat config format for ESLint v9
    - Install @typescript-eslint/parser and @typescript-eslint/eslint-plugin
    - Configure TypeScript and JavaScript linting rules
    - Add type: 'module' to package.json for ES modules support
    - Ignore build directories (.next/, node_modules/, etc.)
    - Set up proper linting for React/TypeScript project

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    ignores: [
+      '.next/**',
+      'node_modules/**',
+      '.vscode/**',
+      'dist/**',
+      'build/**',
+    ],
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      'no-unused-vars': 'off', // Turn off base rule
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+];
